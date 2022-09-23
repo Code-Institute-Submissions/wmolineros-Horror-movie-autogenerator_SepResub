446bf7b148b77a1c4cf9d673a0fe95b12488b51a
wmolineros <wmolineros@gmail.com>
2022-09-23 17:47:04 +0000

    Add button connection to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,22 @@ const homeDetails = document.querySelector('.home-details');
 const assessmentContainer = document.querySelector('.assessment-container');
 const questionContainer = document.getElementById('question-container');
 const assessmentEnd = document.getElementById('assessment-end');
-const formDetails = document.querySelector('.form-details')
+const formDetails = document.querySelector('.form-details');
 const previousRecordContainer = document.getElementById('previous-record-container');
+
+/* Rules of the game */
+
+const rules = document.getElementById('rules');
+const rulesOfTheGame = document.getElementById('rules-of-game');
+const details = document.getElementById('details');
+
+/* Connecting to Buttons */
+
+const startButton = document.getElementById('start-button');
+const homeBtn = document.getElementById('home');
+const previousSuggestionButton = document.getElementById('previous-suggestions');
+const beginAgain = document.getElementById('begin-again'); 
+const nextOne = document.getElementById('next-one');
+const toTheEnd = document.getElementById('to-the-end-btn');
+const reviewQuestions = document.getElementById('review-questions')
 
